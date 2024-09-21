d316df088a5d92356e185079563a7b21f2006f68
erent <erentepe27@gmail.com>
2024-09-21 17:07:37 +0200

    take this

---
+++ src/main/java/net/eren/tuermehanoi/ModClient.java
@@ -0,0 +1,10 @@
+package net.eren.tuermehanoi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
